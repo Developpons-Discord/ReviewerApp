a8a1b7b7507514e88f8eb5cba3b4b637ebacb144
Tom BUTIN <tom.frefre.13@gmail.com>
2023-07-23 22:21:56 +0200

    Added a favicon and axios

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script>
-  import "../app.scss";
+	import '../app.scss';
+	import logo from '$lib/images/logo2.png';
 </script>
 
+<svelte:head>
+	<link rel='shortcut icon' href={logo} type='image/x-icon'>
+</svelte:head>
+
 <main class='h-screen w-screen dark'>
-  <slot />
+	<slot />
 </main>
 
 <style>
