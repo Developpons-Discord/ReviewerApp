cc7925e5967995bda1ef934f7fcc6e8cf040e70a
Welpike <welpike@protonmail.com>
2023-08-06 13:17:08 +0200

    fix tabs +page.svelte

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
     import { TabItem, Tabs } from 'flowbite-svelte';
-	import UserInfo from './UserInfo.svelte';
-	import Feed from './Feed.svelte';
+    import UserInfo from './UserInfo.svelte';
+    import Feed from './Feed.svelte';
 
-	export let data
+    export let data
 </script>
 
 <svelte:head>
-	<title>Dashboard</title>
+    <title>Dashboard</title>
 </svelte:head>
 
 <Tabs style='underline'>
-	<TabItem open title="Feed">
-		<Feed />
-	</TabItem>
-	<TabItem title='Informations'>
-		<UserInfo userData={data.userData} />
-	</TabItem>
+    <TabItem open title="Feed">
+    	<Feed />
+    </TabItem>
+    <TabItem title='Informations'>
+    	<UserInfo userData={data.userData} />
+    </TabItem>
 </Tabs>
