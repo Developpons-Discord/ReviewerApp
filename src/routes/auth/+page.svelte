1b6e77f34500642a5c2847270ea763528e7c8566
Welpike <welpike@protonmail.com>
2023-08-06 13:15:47 +0200

    fix tabs +page.svelte

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,18 +1,18 @@
 <script>
-	import { TabItem, Tabs } from 'flowbite-svelte';
-	import ForgotPassword from './ForgotPassword.svelte';
-	import Login from './Login.svelte';
-	import Register from './Register.svelte';
+    import { TabItem, Tabs } from 'flowbite-svelte';
+    import ForgotPassword from './ForgotPassword.svelte';
+    import Login from './Login.svelte';
+    import Register from './Register.svelte';
 </script>
 
 <Tabs style='underline'>
-	<TabItem open title='Se connecter'>
-		<Login />
-	</TabItem>
-	<TabItem title="S'inscrire">
-		<Register />
-	</TabItem>
-	<TabItem title='Mot de passe oublié'>
-		<ForgotPassword />
-	</TabItem>
+    <TabItem open title='Se connecter'>
+    	<Login />
+    </TabItem>
+    <TabItem title="S'inscrire">
+    	<Register />
+    </TabItem>
+    <TabItem title='Mot de passe oublié'>
+    	<ForgotPassword />
+    </TabItem>
 </Tabs>
