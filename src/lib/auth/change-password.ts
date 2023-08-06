209c4ba184ca05de7a1e2c9901e2e29f6405ea06
Welpike <welpike@protonmail.com>
2023-08-06 17:29:13 +0200

    fix tabs change-password.ts

--- src/lib/auth/change-password.ts
+++ src/lib/auth/change-password.ts
@@ -3,34 +3,34 @@ import Maybe from '$lib/types/maybe';
 
 
 export async function sendEmail(email: string): Promise<Maybe<void, string>> {
-	try {
-		await axios.post('/auth/change-password', {
+    try {
+    	await axios.post('/auth/change-password', {
             email: email
         }, undefined);
-		return Maybe.empty();
-	} catch (e) {
-		const error: string = (e as AxiosError).response?.data?.message;
-		return Maybe.error(error);
-	}
+    	return Maybe.empty();
+    } catch (e) {
+    	const error: string = (e as AxiosError).response?.data?.message;
+    	return Maybe.error(error);
+    }
 }
 
 export async function doChangePassword(
-	userId: number,
-	code: string,
-	password: string,
-	confirmPassword: string
+    userId: number,
+    code: string,
+    password: string,
+    confirmPassword: string
 ): Promise<Maybe<unknown, string[]> | Maybe<void, unknown> | Maybe<unknown, string>>{
     if (password !== confirmPassword) {
-		return Maybe.error(['Le mot de passe et sa confirmation doivent être identiques']);
-	}
+    	return Maybe.error(['Le mot de passe et sa confirmation doivent être identiques']);
+    }
 
     try {
-		await axios.post(`/auth/change-password-process?code=${code}&userId=${userId}`, {
+    	await axios.post(`/auth/change-password-process?code=${code}&userId=${userId}`, {
             new_password: password
         }, undefined);
-		return Maybe.empty();
-	} catch (e) {
-		const error: string = (e as AxiosError).response?.data?.message;
-		return Maybe.error(error);
-	}
+	    return Maybe.empty();
+    } catch (e) {
+    	const error: string = (e as AxiosError).response?.data?.message;
+    	return Maybe.error(error);
+    }
 }
